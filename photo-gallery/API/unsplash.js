5e7f5d714e33a60952274439dd56887d794d8af4
Liudmila <lyudmilaefremova@yandex.ru>
2019-12-30 16:57:56 +0100

    Update unsplash.js

--- photo-gallery/API/unsplash.js
+++ photo-gallery/API/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com/',
     headers: {
-        Authorization: 'Client-ID 2a4562ca9a76d4d2946ee582db07e81e4d628d224ca7b624dce4bf991044b559'
+        Authorization: 'Client-ID'
       }
 });
